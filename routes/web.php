697e50f4e36a7b981b7fe7c14d47d9afe956bd54
alyssacazarte <leenunuyaa@gmail.com>
2023-07-22 09:38:42 +0800

    Fifth Commit

--- routes/web.php
+++ routes/web.php
@@ -23,13 +23,13 @@ Route::get('/', function () {
 
 Route::get('/contact', [ContactController::class, 'index'])->name('contact.index');
 Route::post('/contact/book', [ContactController::class, 'book'])->name('contact.book');
-Route::get('/thank-you', [ContactController::class, 'thankyou'])->name('contact.thankyou');
+Route::get('/thankyou', [ContactController::class, 'thankyou'])->name('contact.thankyou');
 Route::post('/update/{req}', [RequestController::class, 'updateStatus'])->name('request.form');
 Route::get('/form', [RequestController::class, 'index'])->name('contact.form');
 Route::get('/schedule', [ScheduleController::class, 'index']);
 Route::post('/schedule/update/{id}', [ScheduleController::class, 'update'])->name('schedule.update');
 Route::post('/schedule/create', [ScheduleController::class, 'create'])->name('schedule.create');
-Route::get('/thank-you', [ScheduleController::class, 'thankyou'])->name('schedule.thankyou');
+Route::get('/schedule/thankyou', [ScheduleController::class, 'thankyou'])->name('schedule.thankyou');
 Route::get('/success', [ScheduleController::class, 'notification'])->name('schedule.notification');
 
 
